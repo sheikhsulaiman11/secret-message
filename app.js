f9e275a6288e50d96d120f43d32764a7d25f78d7
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-02 12:07:37 +0530

    fixed app.js

--- app.js
+++ app.js
@@ -5,35 +5,24 @@ import messageRoutes from './routes/messageRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 const app = express();
 
+app.set('view engine', 'ejs');
 app.set('trust proxy', 1);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', messageRoutes);
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log('MongoDB connected')
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
             console.log('Server running')
         })
     })
     .catch((err) => {
         console.log('MongoDB connection error:', err)  
     })
-  
-app.set('view engine', 'ejs');
-
-
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use('/messages', messageRoutes);
-app.use('/', messageRoutes);
-
-
-
-app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
-  console.log(`Server is running on port ${process.env.PORT || 8000}`);
-});
