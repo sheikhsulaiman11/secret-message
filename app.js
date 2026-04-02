f78090c34879987e2f0ec623433782bb943f3fdd
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-02 11:28:45 +0530

    added trust proxy

--- app.js
+++ app.js
@@ -7,17 +7,24 @@ dotenv.config();
 
 
 const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 
-app.use('/messages', messageRoutes);
-app.use('/', messageRoutes);
+app.set('trust proxy', 1);
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('mongoDB connected successfully'))
   .catch((err) => console.log(err));
+  
+app.set('view engine', 'ejs');
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/messages', messageRoutes);
+app.use('/', messageRoutes);
+
 
 
 app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
